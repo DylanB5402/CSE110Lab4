64881aa7a1c66d9204bc5c4c03aae08d6d5a7511
Dylan Barva <d.barva5402@gmail.com>
2022-04-18 18:11:41 -0700

    finish lab4

--- app/src/main/java/edu/ucsd/cse110/lab4/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/lab4/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Optional;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +23,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onShowCounterClicked(View view) {
-        Utilities.showAlert(this, "Button doesn't do anything yet");
+//        Utilities.showAlert(this, "Button doesn't do anything yet");
+        Intent intent = new Intent(this, CounterActivity.class);
+
+        TextView maxCountView = findViewById(R.id.max_count_view);
+        String maxCountstr = maxCountView.getText().toString();
+        Optional<Integer> maybeMaxCount = Utilities.parseCount(maxCountstr);
+        if (!maybeMaxCount.isPresent()) {
+            Utilities.showAlert(this, "not a number");
+            return;
+        }
+
+        int maxCount = maybeMaxCount.get();
+        if (maxCount <= 0) {
+            Utilities.showAlert(this, "please enter a pos number");
+            return;
+        }
+//        Utilities.showAlert(this, "I got the number " + maxCount);
+        intent.putExtra("max_count", maxCount);
+        startActivity(intent);
     }
+
 }
